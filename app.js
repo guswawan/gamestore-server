0b942b86e0937512e514bd84f2cf533070c71908
guswawan <agsn.does6@gmail.com>
2022-05-24 18:55:08 +0700

    added cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var methodOverride = require('method-override');
 var flash = require('connect-flash');
 var session = require('express-session');
+var cors = require('cors');
 
 const dashboardRouter = require('./app/dashboard/router');
 const categoryRouter = require('./app/category/router');
@@ -20,6 +21,7 @@ const authRouter = require('./app/auth/router');
 
 const app = express();
 const URL = '/api/v1';
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
